1c3c17684c1732ebab6daaf0e9a14d78f51feafc
Mateo Marin <mateomarin977@gmail.com>
2021-03-08 16:32:52 +0100

    Add Pokemon list
    add the variable PokemonList and a list of Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,35 @@
+// Pokemon List
 
+let pokemonList = [{
+    name: 'Magnemite',
+    height: '0.3',
+    type: ['electric', 'steel'],
+  },
+  {
+    name: 'Squirtle',
+    height: '1.5',
+    type: ['water', 'poison'],
+  },
+  {
+    name: 'Blastoise',
+    height: '1.6',
+    type: ['water', 'poison'],
+  },
+  {
+    name: 'Charmander',
+    height: '0.6',
+    type: ['fire'],
+  },
+  {
+    name: 'Charmeleon',
+    height: '1.1',
+    type: ['fire'],
+  },
+  {
+    name: 'Charizard',
+    height: '1.7',
+    type: ['fire'],
+  },
 
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+]
+document.write(pokemonList);
