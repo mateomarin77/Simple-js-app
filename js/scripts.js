bab901c627b16ecae91d8914494764458528f7dc
Mateo Marin <mateomarin977@gmail.com>
2021-03-11 14:07:30 +0100

    Update JS and CSS
    add loop to pokemon list and style CSS

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,53 @@
-// Pokemon List
+  // Pokemon List Variable
 
-let pokemonList = [{
-    name: 'Magnemite',
-    height: '0.3',
-    type: ['electric', 'steel'],
-  },
-  {
-    name: 'Squirtle',
-    height: '1.5',
-    type: ['water', 'poison'],
-  },
-  {
-    name: 'Blastoise',
-    height: '1.6',
-    type: ['water', 'poison'],
-  },
-  {
-    name: 'Charmander',
-    height: '0.6',
-    type: ['fire'],
-  },
-  {
-    name: 'Charmeleon',
-    height: '1.1',
-    type: ['fire'],
-  },
-  {
-    name: 'Charizard',
-    height: '1.7',
-    type: ['fire'],
-  },
+  let pokemonList = [{
+      name: 'Magnemite',
+      height: '0.3',
+      type: ['electric', 'steel'],
+      weight: ['20'],
+    },
+    {
+      name: 'Squirtle',
+      height: '1.5',
+      type: ['water', 'poison'],
+      weight: ['50'],
+    },
+    {
+      name: 'Blastoise',
+      height: '1.6',
+      type: ['water', 'poison'],
+      weight: ['12'],
+    },
+    {
+      name: 'Charmander',
+      height: '0.6',
+      type: ['fire'],
+      weight: ['1020'],
+    },
+    {
+      name: 'Charmeleon',
+      height: '1.1',
+      type: ['fire'],
+      weight: ['40'],
+    },
+    {
+      name: 'Charizard',
+      height: '1.7',
+      type: ['fire'],
+      weight: ['520'],
+    },
 
-]
-document.write(pokemonList.map((poke) => poke.type));
+  ]
+
+  // Pokemon Loop and write the list to HTML
+  // Highlight special Pokemons by size
+
+  for (let i=0; i < pokemonList.length; i++){
+    if (pokemonList[i].weight > 500 && pokemonList[i].weight < 1000) {
+      document.write(`${(pokemonList[i].name)} - Height: ${pokemonList[i].height}cm Weight: ${pokemonList[i].weight} lbs  <br/><br/><strong> THIS IS A HEAVY POKEMON!!!</strong> </span><br/> <br/>`);
+    } else if (pokemonList[i].weight > 1000) {
+      document.write(`${(pokemonList[i].name)} - Height: ${pokemonList[i].height}cm Weight: ${pokemonList[i].weight} lbs  <br/><br/><strong>THIS IS THE HEAVIEST POKEMON!!! </strong><br/> <br/>`);
+    } else {
+      document.write(`${(pokemonList[i].name)} - Height: ${pokemonList[i].height}cm Weight: ${pokemonList[i].weight} lbs <br/> <br/>`);
+    }
+  };
